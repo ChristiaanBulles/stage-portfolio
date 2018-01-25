791f9abeb55ed227fafbfb5d27e8febde6793e4b
christiaan <chrimar9@live.nl>
2018-01-25 11:25:31 +0100

    Slideshow :)

--- src/Components/Template/index.js
+++ src/Components/Template/index.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Template extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			slide: 0
+		}
+	}
+
+	prevSlide() {
+		if (this.state.slide > 0) {
+			this.setState({
+				slide: this.state.slide - 1
+			})
+		}
+	}
+
+	nextSlide() {
+		if (this.state.slide < (this.props.image.length - 1)) {
+			this.setState({
+				slide: this.state.slide + 1
+			})
+		}
+	}
+
 	render() {
 		const { title, paragraph, image } = this.props
 		return (
@@ -19,8 +42,28 @@ export default class Template extends Component {
 					<h5>2017/2018</h5>
 				</div>
 
-				<div className="Photograph"> 
-					<img src={image} />
+				<div className="Photograph">
+					<div className="slides">
+					{
+						image[this.state.slide] &&
+						<div 
+							className ="slide"
+							style={{
+								backgroundImage: `url(${image[this.state.slide]})`
+							}}>
+
+						</div>
+					}
+					</div>
+
+					<div 
+						className={`prev-slide slide-arrow ${this.state.slide === 0 ? 'deactive' : ''}`}
+						onClick={() => this.prevSlide()}>prev</div>
+
+					<div 
+						className={`next-slide slide-arrow ${this.state.slide === (this.props.image.length - 1) ? 'deactive' : ''}`}
+						onClick={() => this.nextSlide()}>next</div>
+
 				</div>
 				</div>	
 		)
--- src/Data/index.js
+++ src/Data/index.js
@@ -4,7 +4,16 @@ const data = {
 		{
 			"title": "echo",
 			"paragraph": "Wat leuk dat jij een bezoekje brengt op mijn portfolio website.Ik zal mij even kort voorstellen mijn naam is Christiaan Bulles, Ik ben 21 jaar jong en woon in Vaals. Sinds 2015 volg ik de opleiding Media vormgeving op het Arcus in Sittard. Ik zit nu in het derde leerjaar en zit nog steeds vol met passie voor de wereld van design.Op deze site vind je alle projecten die ik de afgelopen 6 maanden heb mogen meelopen. In de afgelopen maanden dat ik stage heb gelopen bij BobWassermann Design mag ik met zekerheid zeggen dat ik enorm ben doorgegroeid en vooral meer zelfvertrouwen heb gekregen.",
-			"image": require('../Images/Echo/ECHO.png')
+			"image": [
+				require('../Images/Echo/Image1.png'),
+				require('../Images/Echo/Image2.png'),
+				require('../Images/Echo/Image3.png'),
+				require('../Images/Echo/Image4.png'),
+				require('../Images/Echo/Image5.png'),
+				require('../Images/Echo/Image6.png'),
+				require('../Images/Echo/Image7.png'),
+				require('../Images/Echo/Image8.png'),
+			]
 		},
 		{
 			"title": "Aisler",
