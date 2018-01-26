4899fcadc61fc8ec7896f1792e507681403bbb3f
christiaan <chrimar9@live.nl>
2018-01-26 20:57:43 +0100

    Mainpage adjustments

--- src/Components/Mainpage/index.js
+++ src/Components/Mainpage/index.js
@@ -6,12 +6,15 @@ export default class Mainpage extends Component {
 		return (
 			<div className="Chris">
 				<div className="person">
-					<h1>CHRISTIAAN BULLES</h1>
-						<div className="scroll">
-						<h2>2017/2018</h2>
-						<h2>SCROLL NAAR BENEDEN</h2>
+					<div className="content">
+						<h1 className="naam">
+						CHRISTIAAN BULLES
+						</h1>
+
+						<h2 className="year">2017/2018</h2>
+						<h2 className="scroll">SCROLL NAAR BENEDEN</h2>
 						<div className="introtekst">Wat leuk dat jij een bezoekje brengt op mijn portfolio website.Ik zal mij even kort voorstellen mijn naam is Christiaan Bulles, Ik ben 21 jaar jong en woon in Vaals. Sinds 2015 volg ik de opleiding Media vormgeving op het Arcus in Sittard. Ik zit nu in het derde leerjaar en zit nog steeds vol met passie voor de wereld van design.Op deze site vind je alle projecten die ik de afgelopen 6 maanden heb mogen meelopen. In de afgelopen maanden dat ik stage heb gelopen bij BobWassermann Design mag ik met zekerheid zeggen dat ik enorm ben doorgegroeid en vooral meer zelfvertrouwen heb gekregen.</div>
-						</div>	
+					</div>
 			</div>
 			</div>
 		)
